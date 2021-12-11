a84c8722e5f920c08e45604b067008eccca19e03
kanlaya <film_kanlaya@hotmail.com>
2021-12-11 14:46:08 +0700

    init management server

---
+++ management_server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send("hello world")
+});
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Listening on port:${port}...`) );
